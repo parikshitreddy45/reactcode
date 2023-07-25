c7e6a23fdf7859a4a9243fb318103f1678a9280a
parikshitreddy45 <127028393+parikshitreddy45@users.noreply.github.com>
2023-07-25 10:36:58 +0530

    Add files via upload

---
+++ listing/listingDisplay.js
@@ -0,0 +1,75 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+
+
+const ListingDisplay = (props) =>{
+
+    const renderData = ({listData}) =>{
+      if(listData){ 
+        if(listData.length>0){
+            return listData.map((item) => {
+               return(       
+                <div className="item" key={item._id}>
+                    <div className="row">
+                        <div className="col-md-5">
+                        <div className="design"><p>THEATRE</p></div> 
+                           <img src={item.Theatre_thumb} alt={item.Theatre_name} className="Image"/>
+                           <div className="theatre_name">
+                                {item.Theatre_name}
+                           <div className="city_name">{item.location_name}</div><br/><br/>
+                        </div>
+                        </div>    
+                        <div>
+                        <div className="design"><p>PRESENT MOVIES</p></div> 
+                            {/* <div className="Tilecontainer"> 
+                            <div className="tileComponent1">
+                                   {item.Movies[0].Movie_Title}<br/>  
+                                    <img src={item.Theatre_thumb} alt={item.Theatre_name} className="rounded float-left"/>
+                                   {item.Movies[1].Movie_Title}<br/>
+                                    <img src={item.Theatre_thumb} alt={item.Theatre_name} className="rounded float-right"/>
+                                </div>
+                           </div>  */}
+                           <div class="row">
+                                <div class="column movie1">
+                                   <img src={item.Movie_img1} alt={item.Movie_Title}/>
+                                   <Link to ={'/details:'}>
+                                        <button>BOOK TICKETS</button>
+                                   </Link>
+                                </div>
+                                <div class="column movie2">
+                                <img src={item.Movie_img2} alt={item.Movie_Title}/>
+                                <Link to ={`'/details:'`}>
+                                        <button>BOOK TICKETS</button>
+                                </Link>
+                                </div>
+                           </div>     
+                        </div>
+                    </div>
+
+                </div>
+               )
+            })
+        }else{
+            return(
+                <h2>No Data Found</h2>
+            )
+        }
+    }else {
+        return(
+                <div>
+                    <h2 className="logostyle">Loading</h2>
+                    <img src="/images/loader.gif" alt="loader"/>
+                </div>
+             ) 
+          }
+    }    
+    return(
+        <div id="content"> 
+            {renderData(props)}
+        </div>
+    ) 
+    }
+
+
+export default ListingDisplay
---
+++ listing/listinglogic.js
@@ -0,0 +1,34 @@
+import React,{useState,useEffect} from "react";
+import './listing.css';
+import { useParams } from "react-router-dom";
+import ListingDisplay from "./listingDisplay";
+import axios from 'axios';
+
+const base_url ="https://bookmytic.onrender.com";
+
+const Listing = () =>{
+    let params = useParams();
+    const [TheatreList,settheatreList] =useState();
+
+    useEffect(() =>{
+        let movieid = params.Movie_id;
+        axios.get(`${base_url}/Category?MovieId=${movieid}`)
+        .then((res) =>{
+            settheatreList(res.data)
+        })
+    },[])
+    
+    return (
+        <>
+            <div className='row'>
+                <div id='mainListing'>
+                    <ListingDisplay listData={TheatreList}/>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Listing
+
+
